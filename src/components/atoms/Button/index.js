154c75d9d9018fabf42d91466478fa2177234b6b
Alfan Fauzy <zhafhy@gmail.com>
2023-01-09 21:57:36 +0700

    feat(TodoAPP): Add button to link github

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { Button } from 'antd';
+import { styled } from '@stitches/react';
 
 /**
  * @param {{
- * type: 'primary' | 'text'
+ * type: 'primary' | 'text' | 'link'
  * icon: React.ReactNode
+ * variant: string
+ * onClick: () => {}
  * }} props
  * @returns
  */
 const AtomButton = (props) => {
   const { type, icon, onClick } = props;
 
-  return <Button type={type} icon={icon} onClick={onClick} />;
+  return (
+    <StyledButton type={type} icon={icon} onClick={onClick} variant={type} />
+  );
 };
+
+const StyledButton = styled(Button, {
+  variants: {
+    variant: {
+      link: {
+        backgroundColor: '#ffe5cc',
+        color: 'black',
+      },
+    },
+  },
+});
 
 export default AtomButton;
